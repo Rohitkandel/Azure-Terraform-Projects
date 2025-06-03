b15e4a71a48808c101b32bbb21941e96f903ac9d
Rohitkandel <59408873+Rohitkandel@users.noreply.github.com>
2025-06-03 18:13:12 +0545

    Update provider.tf

--- vnet-asg-nsg/provider.tf
+++ vnet-asg-nsg/provider.tf
@@ -1,5 +1,4 @@
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
+
 terraform {
   required_providers {
     azurerm = {
@@ -11,6 +10,5 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
- # resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
